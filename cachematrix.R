da858c76db1f454b4b386f5f5487ca32870d2f7d
Chechoblues <sa.galvez10@uniandes.edu.co>
2020-06-21 19:17:04 -0500

    Done!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#At first, I'm gonna set input n as a matrix, with their respective dimension, from a random numerical values.
+#Secondly, I'll set "r" (resoluted value) as NULL, to prevent it's Inverted return by  now
+#Lastly, I'll follow the example changing "mean" for "result"
+makeCacheMatrix <- function(n = matrix(rnorm(24),6,4)) {
+  r <- NULL
+  set <- function(p) {
+    m <<- p
+    r <<- NULL
+  }
+  get <- function() n
+  setResult <- function(solve) r <<- solve
+  getResult <- function() r
+  list(set = set, get = get,
+       setResult = setResult,
+       getResult = getResult)
 }
 
-
-## Write a short comment describing this function
-
+#In the second part, I conserve the same changes
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  r <- m$getResult()
+  if(!is.null(r)) {
+    message("Getting Resoluted Matrix")
+    return(r)
+  }
+  data <- x$get()
+  r <- solve(data, ...)
+  m$setResult(r)
+  r
 }
